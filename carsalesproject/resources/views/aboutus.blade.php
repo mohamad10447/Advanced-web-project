2d2db4e3ed1a133a042a9613858799e73e6f1636
roubeel <roubeelthebian.9@gmail.com>
2024-11-06 15:22:18 +0200

    project main file

---
+++ carsalesproject/resources/views/aboutus.blade.php
@@ -0,0 +1,16 @@
+@extends('layout')
+
+@section('title', 'AboutUs') <!-- Custom title for the page -->
+
+@section('content')
+
+
+
+
+
+
+
+
+
+
+@endsection
---
+++ carsalesproject/routes/web.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/login', function () {
+    return view('login');
+});
+Route::get('/contact', function () {
+    return view('contact');
+});
+Route::get('/aboutus', function () {
+    return view('aboutus');
+});
+
